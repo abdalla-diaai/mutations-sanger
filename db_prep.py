e7df4bd594ad6918c22e3f69bbccd7240bb94b4f
Abdalla Diaai <abdalla.diaai@outlook.com>
2025-04-26 11:23:54 +0100

    initial commit.

---
+++ db_prep.py
@@ -0,0 +1,70 @@
+import polars as ps
+import sqlite3
+import pandas as pd
+
+# model and profiles tables
+model = ps.read_csv("db/model_list_latest.csv")
+
+model.write_database(
+  "Model",
+  "sqlite:///db/ccle.db",
+  if_table_exists = "replace",
+  engine = "adbc",
+)
+
+# somatic mutations table
+csv = ps.read_csv("db/mutations_all_latest.csv")
+columns_to_keep = ["model_id", "gene_symbol", "ensembl_gene_id", "model_name", "cdna_mutation", "protein_mutation","type", "effect", "vaf", "data_type"]
+
+csv = csv.select(columns_to_keep)
+
+csv.write_database(
+  "SomaticMutations",
+  "sqlite:///db/ccle.db",
+  if_table_exists = "replace",
+  engine = "adbc",
+)
+
+# helper function to extract gene names from somatic mutations dataset
+def get_column_values(df, column1):
+  return (df[column1]).to_list()
+
+
+gene_names = get_column_values(csv, "HugoSymbol")
+
+# convert to dataframe
+g2t = ps.DataFrame(
+  {
+    "Gene": [x for x in gene_names],
+  }
+)
+
+# write to sqlite3 database
+g2t.write_database("Genes", "sqlite:///db/ccle.db", if_table_exists = "replace", engine="adbc")
+
+# connect to dataset
+con = sqlite3.connect("db/ccle.db")
+# Optimize database
+con.execute("PRAGMA synchronous = OFF;")
+con.execute("PRAGMA journal_mode = MEMORY;")
+con.execute("PRAGMA temp_store = MEMORY;")
+# Perform ANALYZE to update statistics
+con.execute("ANALYZE;")
+# Perform VACUUM to reduce database size
+con.execute("VACUUM;")
+# Index columns
+con.execute("CREATE INDEX ix_gt_gene ON Genes(Gene)")
+con.execute("CREATE INDEX ix_mod_profile ON Model(ProfileID)")
+con.execute("CREATE INDEX ix_mod_cln ON Model(CellLineName)")
+
+# Create views
+con.execute(
+  """
+  CREATE VIEW view_unique_genes
+  AS
+    SELECT DISTINCT Gene FROM Genes ORDER BY Gene
+  """
+)
+
+con.close()
+
